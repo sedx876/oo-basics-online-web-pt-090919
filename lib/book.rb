0d7b843b2397dbbf12a3329dfe7bfb266e6d71c6
Sharon Watkins <swatkins876@gmail.com>
2019-09-21 19:04:26 +0000

    Automatically backed up by Learn

--- lib/book.rb
+++ lib/book.rb
@@ -23,6 +23,14 @@ class Book
   def page_count
     @page_count
   end
+  
+  def genre=(genre)
+    @genre = genre
+  end
+ 
+  def genre
+    @genre
+  end
  
 end
 Book.new("And Then There Were None")
